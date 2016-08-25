4e64cf03e4a52b5e59d8cdc46be1760d171971df
Del Pede <delpede@gmail.com>
2016-08-25 20:58:19 +0200

    Still working on converting to use peewee. A lot does not work. Getting there with babysteps

--- discord_bot.py
+++ discord_bot.py
@@ -13,13 +13,18 @@ db = SqliteDatabase('discord_bot.db')
 class DiscordName(Model):
     id = IntegerField(primary_key=True)
     discord_name = CharField(unique=True)
-    steam = CharField
-    origin = CharField
-    battlenet = CharField
-    uplay = CharField
+    steam = CharField(null=True)
+    origin = CharField(null=True)
+    battlenet = CharField(null=True)
+    uplay = CharField(null=True)
 
     class Meta:
         database = db
+
+    def create_table_discordname():
+        db.connect()
+        db.create_tables([DiscordName])
+        db.close()
 
 
 class GamerTag():
@@ -37,11 +42,34 @@ class GamerTag():
             # return add_gamertag(self)
 
         db.connect()
-        db.create_tables([DiscordName])
 
-        result = DiscordName.create(discord_name=dn, steam=gt)
+        if gp == "steam":
+            result = DiscordName.create(discord_name=dn, steam=gt)
+        elif gp == "origin":
+            result = DiscordName.create(discord_name=dn, origin=gt)
+        elif gp == "battlenet":
+            result = DiscordName.create(discord_name=dn, battlenet=gt)
+        elif gp == "uplay":
+            result = DiscordName.create(discord_name=dn, uplay=gt)
+
+        db.close()
+
+
+    def list_gamertag(self):
+
+        dn = str(input("Type Discord Name: "))
+
+        db.connect()
+
+        try:
+            result = DiscordName.get(DiscordName.discord_name == dn)
+            return result
+        except result.DoesNotExist:
+            print (dn + " does not exist")
+
         db.close()
 
 
 gt = GamerTag()
-gt.add_gamertag()
+# gt.add_gamertag()
+gt.list_gamertag()
